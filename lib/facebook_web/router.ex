a9fa9ec9796d6116f63cabc609dfad0e39d00c28
Jake Galagar <jakegalagar22@gmail.com>
2025-12-04 12:38:08 +0800

    Generate User Auth

--- lib/facebook_web/router.ex
+++ lib/facebook_web/router.ex
@@ -1,6 +1,8 @@
 defmodule FacebookWeb.Router do
   use FacebookWeb, :router
 
+  import FacebookWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule FacebookWeb.Router do
     plug :put_root_layout, html: {FacebookWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -40,5 +43,33 @@ defmodule FacebookWeb.Router do
       live_dashboard "/dashboard", metrics: FacebookWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", FacebookWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{FacebookWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", FacebookWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{FacebookWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
